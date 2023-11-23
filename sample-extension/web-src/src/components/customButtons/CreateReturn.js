0a7acfc67161c84400c1150786c4f11bbdbf94cc
Alex Salloum <alexandre.salloum@live.com>
2023-11-23 11:31:29 +0100

    Add possibility to redirect to commerce from CreateReturn button

--- sample-extension/web-src/src/components/customButtons/CreateReturn.js
+++ sample-extension/web-src/src/components/customButtons/CreateReturn.js
@@ -1,12 +1,13 @@
 import {attach} from "@adobe/uix-guest";
 import {extensionId} from "../Constants";
 import {useState} from "react";
-import {Flex, Heading, ProgressCircle, View} from "@adobe/react-spectrum";
+import {Button, Flex, Heading, ProgressCircle, View} from "@adobe/react-spectrum";
 
 export const CreateReturn = () => {
 
     const [orderId, setOrderId] = useState(null)
     const [isLoading, setIsLoading] = useState(true)
+    const [connection, setConnection] = useState(undefined)
 
     const getGuestConnection = async () => {
         return await attach({
@@ -14,7 +15,8 @@ export const CreateReturn = () => {
         })
     }
 
-    getGuestConnection().then(() => {
+    getGuestConnection().then((guestConnection) => {
+        setConnection(guestConnection)
         const urlString = window.location.href
         const queryString = urlString.split('?')[1]
         const searchParams = new URLSearchParams(queryString)
@@ -32,6 +34,9 @@ export const CreateReturn = () => {
                 <View margin={10}>
                     <Heading level={1}>Request return </Heading>
                     orderId is: {orderId}
+                    <Button variant={"primary"} marginStart={10} onPress={async () => { await connection.host.field.close()}}>
+                        Done
+                    </Button>
                 </View>
             )}
         </View>
